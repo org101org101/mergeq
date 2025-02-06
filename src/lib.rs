f56e6714d47a194fd825d541a6499cbeba2e8650
yihau <yihau.chen@icloud.com>
2025-02-06 23:37:11 +0800

    rename add

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
+pub fn plus(left: u64, right: u64) -> u64 {
     left + right
 }
 
@@ -12,7 +12,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = plus(2, 2);
         assert_eq!(result, 4);
     }
 }
