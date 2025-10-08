bf1e7618a2396fa54d0b8c57e8feda2647ba1ed3
dandeliontheking <117677935+dandeliontheking@users.noreply.github.com>
2025-10-08 13:27:49 +0000

    update

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -4,25 +4,25 @@ echo "BUILDKITE_BRANCH: $BUILDKITE_BRANCH"
 
 if [[ $BUILDKITE_BRANCH == gh-readonly-queue* ]]; then
   cat <<EOF | tee /dev/tty | buildkite-agent pipeline upload
-  steps:
-    - name: "main"
-      command: "./check.sh"
-      timeout_in_minutes: 20
-      agents:
-        queue: "default"
-    - name: "main"
-      command: "./check.sh"
-      timeout_in_minutes: 20
-      agents:
-        queue: "default"
+steps:
+  - name: "main"
+    command: "./check.sh"
+    timeout_in_minutes: 20
+    agents:
+      queue: "default"
+  - name: "main"
+    command: "./check.sh"
+    timeout_in_minutes: 20
+    agents:
+      queue: "default"
 EOF 
 else
   cat <<EOF | tee /dev/tty | buildkite-agent pipeline upload
-  steps:
-    - name: "main"
-      command: "./check.sh"
-      timeout_in_minutes: 20
-      agents:
-        queue: "default"
+steps:
+  - name: "main"
+    command: "./check.sh"
+    timeout_in_minutes: 20
+    agents:
+      queue: "default"
 EOF
 fi
