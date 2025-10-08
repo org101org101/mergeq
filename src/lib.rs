5ddb774a6811b20f12bb3a1fe41c7e5cdb0b734e
deantheking <117676136+deantheking@users.noreply.github.com>
2025-10-08 01:52:33 +0000

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,7 @@
-pub fn plus(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-pub fn sub(left: u64, right: u64) -> u64 {
-    left - right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
-        let result = plus(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn it_works2() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(4, 2 + 2);
     }
 }
